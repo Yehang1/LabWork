a3154abd334fe88a39d4601d0d1c0cf4337fe843
Yehang10 <yehangrai10@gmail.com>
2021-03-09 09:27:35 +0545

    question 6

---
+++ practice/six.py
@@ -0,0 +1,6 @@
+"print the body mass index"
+#formula: BMI=mass(in kg)/height (in meter^2)
+mass=float(input("enter the mass of person in kg:"))
+height=float(input("enter the height of a person in meter: "))
+BMI=(mass/(height*height))
+print("the body mass index of the person is",BMI)
