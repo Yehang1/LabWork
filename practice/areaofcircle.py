1c11a6f66ddd7379de0e3fd318da817d2e9b7302
Yehang10 <yehangrai10@gmail.com>
2021-03-23 09:18:54 +0545

    branching practice

--- practice/areaofcircle.py
+++ practice/areaofcircle.py
@@ -1,5 +1,2 @@
-#wap to find the area of circle
-r=float(input('enter the radius'))
-area=(22/7)*(r**2)
-print("the length of radius is:"r)
-print("the area of the circle is:",area)
+
+print("hello world")
